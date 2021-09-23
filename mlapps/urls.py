8a31ff53083372d31ceb99050a1612e4b210e52c
Your Name <you@example.com>
2021-09-23 19:40:15 +0900

    cssファイルを追加

--- mlapps/urls.py
+++ mlapps/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.entrance, name='entrace'),
     path('score/', views.score, name='score'),
-    path('travel/score/score_detail/', views.score_detail, name='score_detail'),
+    path('travel_detail/score/score_detail/', views.score_detail, name='score_detail'),
     path('rent/', views.rent, name='rent'),
-    path('travel/rent/rent_detail/', views.rent_detail, name='rent_detail'),
+    path('travel_detail/rent/rent_detail/', views.rent_detail, name='rent_detail'),
     path('travel/', views.travel, name='travel'),
     path('travel/travel/travel_detail/', views.travel_detail, name='travel_detail'),
 ]
